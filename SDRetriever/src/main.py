0d4763197ccfb9de7d22f6f63f49f745a8ab453c
Peres Martinho (XC/PJ-RC42) <pem3brg@bosch.com>
2021-09-29 17:37:15 +0100

    Added S3 cross account transfer test

--- SDRetriever/src/main.py
+++ SDRetriever/src/main.py
@@ -6,10 +6,10 @@ from baseaws.shared_functions import ContainerServices
 from datetime import datetime
 
 CONTAINER_NAME = "SDRetriever"    # Name of the current container
-CONTAINER_VERSION = "v2.3"      # Version of the current container
+CONTAINER_VERSION = "v2.5"      # Version of the current container
 
 
-def transfer_kinesis_clip(s3_client, kinesis_client, sts_client, container_services, body):
+def transfer_kinesis_clip(s3_client, sts_client, container_services, body):
     """Converts the message body to json format (for easier variable access),
     gets video clip from RCC Kinesis video stream and stores the received
     clip on the raw data S3 bucket to be later processed by the data
@@ -18,8 +18,6 @@ def transfer_kinesis_clip(s3_client, kinesis_client, sts_client, container_servi
     Arguments:
         s3_client {boto3.client} -- [client used to access
                                      the S3 service]
-        kinesis_client {boto3.client} -- [client used to access
-                                          the Kinesis service]
         sts_client {boto3.client} -- [client used to assume
                                       a given cross-account IAM role]
         container_services {BaseAws.shared_functions.ContainerServices}
@@ -35,7 +33,7 @@ def transfer_kinesis_clip(s3_client, kinesis_client, sts_client, container_servi
     new_body = body.replace("\'", "\"")
     dict_body = json.loads(new_body)
 
-    #############################
+    ##########################################################
     # TODO: CONVERT MSG PARAMETERS TO BE USED ON GET_KINESIS_CLIP FUNCTION
 
     # TEST VALUES 
@@ -46,7 +44,12 @@ def transfer_kinesis_clip(s3_client, kinesis_client, sts_client, container_servi
     stream_arn = "arn:aws:kinesisvideo:eu-central-1:213279581081:stream/TEST_TENANT_INTEGRATION_TEST_DEVICE_InteriorRecorder/1630061769043"
     stream_role = "arn:aws:iam::213279581081:role/dev-datanauts-KVS-Source-Stream-Role"
     sts_session = "AssumeRoleSession1"
-    #############################
+    # TODO: DEFINE S3 PATH FOR CLIP + DESTINATION FOLDER PROCESSING (CONFIG FILE?)
+    # MESSAGE EXAMPLE:  {"clip_name": "kinesis_clip.mp4", "folder": "lyft", "meta_name": "metadata_full.json"}
+    s3_folder = dict_body['folder'] # 'lyft'
+    s3_filename = dict_body['clip_name']  # 'kinesis_clip.mp4'
+    s3_path = s3_folder + '/' + s3_filename
+    ##########################################################
 
     # Requests credentials to assume specific cross-account role
     assumed_role_object = sts_client.assume_role(RoleArn=stream_role,
@@ -61,9 +64,6 @@ def transfer_kinesis_clip(s3_client, kinesis_client, sts_client, container_servi
                                                      start_time,
                                                      end_time,
                                                      selector)
-
-    # TODO: DEFINE S3 PATH FOR CLIP
-    s3_path = 'lyft/test_clip.mp4'
 
     # Upload video clip into raw data S3 bucket
     container_services.upload_file(s3_client,
@@ -92,18 +92,19 @@ def concatenate_metadata_full(s3_client, sts_client, container_services, body):
 
     # Converts message body from string to dict
     # (in order to perform index access)
-    '''
     new_body = body.replace("\'", "\"")
     dict_body = json.loads(new_body)
-    '''
+
     #############################
-    # TODO: CONVERT MSG PARAMETERS TO BE USED ON GET_KINESIS_CLIP FUNCTION
+    # TODO: CONVERT MSG PARAMETERS TO BE USED ON THIS FUNCTION
 
     # TEST VALUES
-    bucket_origin = 'dev-rcd-config-files'
-    bucket_storage = 'dev-rcd-config-files'
 
-    key_prefix = 'test/InteriorRecorder_InteriorRecorder-62c86acc-3c3b-4d76-b00f-037fcd82021'
+    bucket_origin = 'rcc-dev-device-data'
+
+    # MESSAGE EXAMPLE:  {"clip_name": "kinesis_clip.mp4", "folder": "lyft", "s3_key_prefix": "honeybadger/ivs_srx_develop_tmk2si_03/year=2021/month=09/day=22/hour=05/InteriorRecorder_InteriorRecorder-8a79e911-deb6-4d1d-8c52-372f8eb07e49"}
+    key_prefix = dict_body['s3_key_prefix'] # 'test/InteriorRecorder_InteriorRecorder-62c86acc-3c3b-4d76-b00f-037fcd82021'
+    
     # name of the folder and file for the final concatenated file
     key_full_metadata = 'uber/InteriorRecorder_InteriorRecorder-62c86acc-3c3b-4d76-b00f-037fcd82021_metadata_full.json'
     s3_role = "arn:aws:iam::213279581081:role/dev-datanauts-KVS-Source-Stream-Role"
@@ -131,7 +132,7 @@ def concatenate_metadata_full(s3_client, sts_client, container_services, body):
         Bucket=bucket_origin,
         Prefix=key_prefix
     )               
-
+    ''' #################### UNCOMMENT THIS PART TO ENABLE CONCATENATION
     # Initialise dictionary that will store all files
     # that match the received prefix
     files_dict = {}
@@ -177,14 +178,32 @@ def concatenate_metadata_full(s3_client, sts_client, container_services, body):
     # Convert concatenated dictionary into json and then into bytes so
     # that it can be uploaded into the S3 bucket
     concatenated_file = (bytes(json.dumps(final_dict, ensure_ascii=False, indent=4).encode('UTF-8')))
-
+    
     # Upload final concatenated file
     container_services.upload_file(s3_client,
                                     concatenated_file,
                                     container_services.raw_s3,
                                     key_full_metadata)
+    '''
 
+    #####################################
+    # CROSS ACCOUNT S3 ACCESS TEST
+    for index, file_entry in enumerate(response_list['Contents']):
 
+        logging.info("%s\n", file_entry['Key'])
+        
+        if file_entry['Key'].endswith('.json'):
+            metadata_file = container_services.download_file(rcc_s3,
+                                                            bucket_origin,
+                                                            file_entry['Key'])
+
+            key_full_metadata = 'uber/' + file_entry['Key']
+
+            container_services.upload_file(s3_client,
+                                            metadata_file,
+                                            container_services.raw_s3,
+                                            key_full_metadata)
+    ######################################
 def main():
     """Main function"""
 
@@ -199,8 +218,6 @@ def main():
                              region_name='eu-central-1')
     sqs_client = boto3.client('sqs',
                               region_name='eu-central-1')
-    kinesis_client = boto3.client('kinesisvideo',
-                                  region_name='eu-central-1')
     sts_client = boto3.client('sts',
                               region_name='eu-central-1')
 
@@ -222,7 +239,6 @@ def main():
             # Get and store kinesis video clip
 
             transfer_kinesis_clip(s3_client,
-                                  kinesis_client,
                                   sts_client,
                                   container_services,
                                   message['Body'])
