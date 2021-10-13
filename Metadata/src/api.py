62bd4bcd47321dc5c1eed0373eb53753540a07bf
Peres Martinho (XC/PJ-RC42) <pem3brg@bosch.com>
2021-10-13 09:41:31 +0100

    Added 1st version of API documentation (Metadata)

--- Metadata/src/api.py
+++ Metadata/src/api.py
@@ -9,6 +9,7 @@ from botocore.exceptions import ClientError
 from flask_cors import CORS
 from pymongo import MongoClient
 import json
+from flask_restx import Api, Resource, reqparse, fields
 
 # Container info
 CONTAINER_NAME = "Metadata"
@@ -16,44 +17,114 @@ CONTAINER_VERSION = "v6.0"
 
 # DocumentDB info
 DB_NAME = "DB_test"
-EXEC_COL_NAME = "dev-pipeline-execution"
-ALGO_COL_NAME = "dev-algorithm-output"
 
 # AWS region 
 REGION_NAME = "eu-central-1"
 
 # API response codes
-ERROR_HTTP_CODE = "500"
-SUCCESS_HTTP_CODE = "200"
-BAD_REQUEST_CODE = "400"
-NOT_FOUND_CODE = "404"
+ERROR_HTTP_CODE = '500'
+SUCCESS_HTTP_CODE = '200'
+BAD_REQUEST_CODE = '400'
+NOT_FOUND_CODE = '404'
 
+# API response messages
+ERROR_400_MSG = 'Invalid or missing argument(s)'
+ERROR_500_MSG = 'Mapping Key Error'
+
+# API instance initialisation
 app = flask.Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+# Swagger documentation initialisation
+api = Api(app, version='1.0', title='Metadata management API',
+    description='API used for communication between Fronted UI and DocumentDB database',
+)
 
-@app.route("/alive", methods=["GET"])
-def alive():
-    """
-    Returns status code 200 if alive
+@api.route('/alive')
+class Alive(Resource):
+    @api.doc(responses={200:'Ok'})
+    def get(self):
+        """
+        Checks if API is alive
+        """
+        return flask.jsonify(message='Ok', statusCode="200")
 
-    Arguments:
-    Returns:
-        flask.jsonify -- [json with the status code + data]
-    """
-    return flask.jsonify(code=SUCCESS_HTTP_CODE, message='Ok')
+@api.route('/ready')
+class Ready(Resource):
+    @api.doc(responses={200:'Ready'})
+    def get(self):
+        """
+        Checks if API is ready
+        """
+        return flask.jsonify(message='Ready', statusCode="200")
 
-@app.route("/ready", methods=["GET"])
-def ready():
-    """
-    Returns status code 200 if ready
 
-    Arguments:
-    Returns:
-        flask.jsonify -- [json with the status code + data]
-    """
-    return flask.jsonify(code=SUCCESS_HTTP_CODE, message='Ready')
+@api.route('/dbStatus')
+class Status(Resource):
+    @api.doc(responses={200:ERROR_400_MSG, 400:ERROR_400_MSG, 500:ERROR_500_MSG})
+    def get(self):
+        """
+        Returns a list of all databases and collections currently present on the DocumentDB cluster
+        ** FOR DEBUG PURPOSES **
+        """
+        try:
+            # Create a MongoDB client, open a connection to Amazon DocumentDB
+            # as a replica set and specify the read preference as
+            # secondary preferred
+            client = MongoClient(docdb_info['cluster_endpoint'], 
+                                 username=docdb_info['username'],
+                                 password=docdb_info['password'],
+                                 tls=docdb_info['tls'],
+                                 tlsCAFile=docdb_info['tlsCAFile'],
+                                 replicaSet=docdb_info['replicaSet'],
+                                 readPreference=docdb_info['readPreference'],
+                                 retryWrites=docdb_info['retryWrites']
+                                )
 
+            response = {}
+
+            # Get list of current databases on the cluster
+            response['dbs_list'] = client.list_database_names()
+
+            # Get list of current collections on each database
+            response['col_list'] = {}
+            for db_name in response['dbs_list']:
+                mydb = client[db_name]
+                response['col_list'][db_name] = mydb.list_collection_names()
+
+            # Close the connection
+            client.close()
+
+            return flask.jsonify(message=response, statusCode="200")
+        except Exception as e:
+            api.abort(400, message=ERROR_400_MSG, statusCode = "400")
+        except KeyError as e:
+            api.abort(500, message=ERROR_500_MSG, statusCode = "500")
+##############################################################################################################
+
+'''
+parser = reqparse.RequestParser()
+parser.add_argument('id', type=str, required=True, help='Top text', location='form')
+
+@api.route('/debugAddItem')
+class MyResource(Resource):
+    @api.doc(responses={200:STATUS_200_MSG, 400:ERROR_400_MSG, 500:ERROR_500_MSG})
+    @api.expect(parser, validate=True)
+    def post(self):
+        """
+        Inserts item in a given collection
+        ** FOR DEBUG PURPOSES **
+
+        """
+        try:
+            str_item = flask.request.form["id"]
+            msg = str_item + "post"
+            return flask.jsonify(message=msg, statusCode="200")
+        except Exception as e:
+            api.abort(400, message=ERROR_400_MSG, statusCode = "400")
+        except KeyError as e:
+            api.abort(500, message=ERROR_500_MSG, statusCode = "500")
+'''
 @app.route("/getAllItems", methods=["POST"])
 def get_all_items():
     """
@@ -340,6 +411,7 @@ def debug_delete_item():
 
             try:
                 # Delete item
+                # TODO: CHECK FIRST IF ITEM EXISTS 
                 col.delete_one(item)
                 # Close the connection
                 client.close()
