45c40579a562035a1e729e131cefe61ce7574371
Peres Martinho (XC-CT/PJ-RC42) <pem3brg@bosch.com>
2022-01-11 13:09:36 +0000

    bug prints

--- ACAPI/src/start_container.sh
+++ ACAPI/src/start_container.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+CONFIG_S3="dev-rcd-config-files"
+echo $CONFIG_S3
 python api.py $CONFIG_S3
--- Metadata/src/start_container.sh
+++ Metadata/src/start_container.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+echo $RCD_EKS_CLUSTER_ID
+echo $region
+echo $METADATA_IAM_ROLE_NAME
 python ./main.py $CONFIG_S3 & python api.py $CONFIG_S3 #& python tests/integration/api_test.py
