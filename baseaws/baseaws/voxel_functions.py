c8a0eccf4bb8b9d79886c34b5b590f38f02d0bd8
EXTERNAL Oliveira Andre (ALTEN <ola4brg@bosch.com>
2022-03-09 10:10:15 +0000

    Update input parameters validatio to try+exeption blocks

--- baseaws/baseaws/voxel_functions.py
+++ baseaws/baseaws/voxel_functions.py
@@ -102,39 +102,103 @@ def update_sample(data_set,sample_info):
 
     dataset = fo.load_dataset(data_set)
 
-    filepath = sample_info["s3_path"]
 
-    sample = dataset[filepath]
-    
+    #IF the sample already exists, update it's information, otherwise create a new one
+
+    try:
+        sample = dataset.one(F("pipeline_id") == sample_info["_id"])
+      
+    except Exception:
+        sample = fo.Sample(filepath=sample_info["s3_path"])    
+
  
-# Parse and populate new metadata
-    meta_avail_bool = sample_info["metadata_available"] == "Yes"
-    sample["metadata_available"] = meta_avail_bool
 
+
+    #  Full create FiftyOne Sample object
+    # 
+    #     filepath = sample_info["s3_path"]
+    #     sample = fo.Sample(filepath=filepath)
+    # 
+    #     # Parse and populate labels and metadata on sample
+
+    #Setup mandatory field
     sample["pipeline_id"] = sample_info["_id"]
-    sample["pipeline_stage"] = sample_info["just created"] #and other stages, to validate where the sample is in terms of processing
 
-    sample["cameraID"] = sample_info["recording_overview"]["cameraID"]
-    sample["recording_resolution"] = sample_info["recording_overview"]["resolution"]
+    #Validate and populate optional fields 
+    try:
+        sample["metadata_available"] = sample_info["metadata_available"]
+    except Exception:
+        logging.info("No metadata_available")
 
-    recording_time = sample_info["recording_overview"]["time"]
-    recording_datetime = datetime.strptime(recording_time, "%Y-%m-%d %H:%M:%S")
-    sample["recording_time"] = recording_datetime
+    try:
+        sample["pipeline_stage"] = sample_info["pipeline_stage"]
+    except Exception:
+        logging.info("No pipeline_stage")
 
-    # Add results as Regression Labels
-    for algo_results in sample_info["results"]:
-        alg = algo_results["algorithm"]
-        source = algo_results["source"]
-        label_field = alg        
-        for ind, chb in enumerate(algo_results["CHBs"]):
-            label = fo.Regression(
-                value=float(chb),
-                source=source,
-                alg=alg,
-            )
-            frame_number = ind+1
-            # frame_number = etaf.timestamp_to_frame_number(timestamp, duration, total_frame_count)
-            sample.frames[frame_number][label_field] = label
+    try:
+        sample["recording_overview"] = sample_info["recording_overview"]
+    except Exception:
+        logging.info("No recording_overview")
 
+    try:
+        sample["cameraID"] = sample_info["recording_overview"]["cameraID"] 
+    except Exception:
+        logging.info("No cameraID")
 
-    sample.save()
+    try:
+        sample["resolution"] = sample_info["recording_overview"]["resolution"] 
+    except Exception:
+        logging.info("No resolution")
+
+    try:
+        recording_time = sample_info["recording_overview"]["time"]
+        recording_datetime = datetime.strptime(recording_time, "%Y-%m-%d %H:%M:%S")
+        sample["recording_time"] = recording_datetime
+    except Exception:
+        logging.info("No time")
+
+    try:
+        sample["data_status"] = sample_info["data_status"]
+    except Exception:
+        logging.info("No data_status")        
+
+    try:
+        sample["from_container"] = sample_info["from_container"]
+    except Exception:
+        logging.info("No from_container")        
+
+    try:
+        sample["info_source"] = sample_info["info_source"]
+    except Exception:
+        logging.info("No info_source")        
+
+    try:
+        sample["last_updated"] = sample_info["last_updated"]
+    except Exception:
+        logging.info("No last_updated")        
+        
+    try:
+        sample["processing_list"] = sample_info["processing_list"]
+    except Exception:
+        logging.info("No processing_list")        
+
+        # 
+#     # Add results as Regression Labels
+#     for algo_results in sample_info["results"]:
+#         alg = algo_results["algorithm"]
+#         source = algo_results["source"]
+#         label_field = alg        
+#         for ind, chb in enumerate(algo_results["CHBs"]):
+#             label = fo.Regression(
+#                 value=float(chb),
+#                 source=source,
+#                 alg=alg,
+#             )
+#             frame_number = ind+1
+#             # frame_number = etaf.timestamp_to_frame_number(timestamp, duration, total_frame_count)
+#             sample.frames[frame_number][label_field] = label
+# 
+# 
+    
+    # Add sample to dataset
+    dataset.add_sample(sample)
