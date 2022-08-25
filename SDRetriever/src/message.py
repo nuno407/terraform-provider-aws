57fef396026446c84e6b5d8de15929681d40999e
Afonso Sousa <afonso.sousa2@bosch.com>
2022-08-25 17:46:50 +0100

    Refactor of SDRetriever, with support for MDFParser (#101)
    * initial SDR refactoring implementation commit
    
    * Refactored SDR
    
    * message identification feature (#71)
    
    * Implements message identification feature
    
    * Update main.py
    
    remove commented identification code
    
    Co-authored-by: Afonso Sousa <afonso.sousa2@bosch.com>
    
    * remove gzip and abc from requirements.txt, add shared_functions import, add SourceCommuter to baseaws
    
    * Update selective-image-build.yml
    
    * Add metadata calculations to create MDF
    
    * Add metadata calculations to create MDF, remove chc_signals from source data
    
    * remove SIGSTOP since create problems with windows users
    
    * Create code modules
    
    * Disable log propagation
    
    * Correct Kinesis timestamps
    
    * Correct snapshot upload, change naming convention to include tenant and deviceid in the name for cross-referencing
    
    * Express metadata ingestion, improve topicarn logging
    
    * Upload TrainingRecorder data, correct metadata upload path, snapshot checks
    
    * Documentation and minor corrections, plus sonarqube complaints
    
    * update branch to continue test to SDR component
    
    * Correct indentation to insert TrainingRecorder videos into DB
    
    * test message
    
    * Test Chunk class
    
    * Test Message base class
    
    * Test VideoMessage and SnapshotMessage
    
    * test message
    
    * Corrections for edge cases found during testing and MDFP integration
    
    * Corrections for edge cases found during testing and MDFP integration, formatting
    
    * Implement ingestor.py unit tests
    
    * Implement tests fot video ingestor, conftest, reduce TestIngestor
    
    * Implement snaphsot ingestor tests
    
    * Metadata unit tests, integration
    
    * Add Jackalope to tenant blacklist
    
    * Add fields to message sent to Metadata, send messages to create snapshot records on db, add snapshot re-ingestion
    
    Co-authored-by: Afonso Sousa <soa5brg@bosch.com>
    Co-authored-by: Anselmo Gonçalves <103199037+AnselmoG@users.noreply.github.com>
    Co-authored-by: Goncalves Anselmo (XC-CT/PJ-RC42) <goa5brg@bosch.com>

---
+++ SDRetriever/src/message.py
@@ -0,0 +1,379 @@
+import json
+import logging as log
+from abc import abstractmethod, abstractproperty
+from datetime import datetime
+from typing import TypeVar
+
+DT = TypeVar("DT",dict, None)
+TENANT_BLACKLIST = {'TEST_TENANT', 'herbie','jackalope'} # Tenants we receive messages from, but don't have access for 
+LOGGER = log.getLogger("SDRetriever")
+
+class Chunk(object):
+    """Representation of a single message chunk object"""
+
+    def __init__(self, chunk_description: dict = dict()) -> None:
+        self.uuid : str = chunk_description.get("uuid")
+        self.upload_status : str = chunk_description.get("upload_status")
+        self.start_timestamp_ms : str = chunk_description.get("start_timestamp_ms")
+        self.end_timestamp_ms : str = chunk_description.get("end_timestamp_ms")
+        self.payload_size : str = chunk_description.get("payload_size")
+        self.available : bool = self.upload_status == "UPLOAD_STATUS__ALREADY_UPLOADED" 
+
+
+    def __repr__(self) -> str:
+        return f"{self.uuid}, [{self.start_timestamp_ms}, {self.end_timestamp_ms}]"
+
+
+class Message(object):
+    """Base class, describes a single, generic SQS message for data ingestion."""
+
+    def __init__(self, sqs_message: dict = None) -> None:
+        """Class constructor
+
+        Args:
+            sqs_message (dict, optional): Message as read from input queues, without any transformations. Defaults to None.
+        """
+        if type(sqs_message) == dict:
+            self.raw_message = sqs_message
+
+    @abstractmethod
+    def validate(self):
+        pass
+
+    @property
+    def messageid(self) -> str:
+        messageid = None
+        if "MessageId" in self.raw_message:
+            messageid = self.raw_message.get("MessageId")
+        else:
+            LOGGER.error("Field 'MessageId' not found in 'InputMessage'", extra={"messageid": messageid})
+        return messageid
+
+    @property
+    def receipthandle(self) -> str:
+        receipthandle = None
+        if "ReceiptHandle" in self.raw_message:
+            receipthandle = self.raw_message.get("ReceiptHandle")
+        else:
+            LOGGER.error("Field 'ReceiptHandle' not found in 'InputMessage'", extra={"messageid": self.messageid})
+        return receipthandle
+
+    @property
+    def attributes(self) -> dict:
+        attributes = {}
+        if "Attributes" in self.raw_message:
+            attributes = self.raw_message.get("Attributes")
+        else:
+            LOGGER.error("Field 'Attributes' not found in 'InputMessage'", extra={"messageid": self.messageid})
+        return attributes
+    @property
+    def body(self) -> dict:
+        body = {}
+        if "Body" in self.raw_message:
+            body = self.raw_message.get("Body")
+            if type(body) == str:
+                body = json.loads(body.replace("\'", "\""))
+        else:
+            LOGGER.error("Field 'Body' not found in 'InputMessage'", extra={"messageid": self.messageid})
+        return body
+
+    @property
+    def message(self) -> dict:
+        message = {}
+        if "Message" in self.body:
+            message = self.body.get("Message")
+            if type(message) == str:
+                message = json.loads(message.replace("\'", "\""))
+        else:
+            LOGGER.error("Field 'Message' not found in 'Body'", extra={"messageid": self.messageid})
+        return message
+
+    @property
+    def messageattributes(self) -> dict:
+        messageattributes = {}
+        if "MessageAttributes" in self.raw_message:
+            messageattributes = self.raw_message.get("MessageAttributes")
+            if type(messageattributes) == str:
+                messageattributes = json.loads(messageattributes.replace("\'", "\""))
+        elif "MessageAttributes" in self.body:
+            messageattributes = self.body.get("MessageAttributes")
+            if type(messageattributes) == str:
+                messageattributes = json.loads(messageattributes.replace("\'", "\""))
+        else:
+            LOGGER.error("Field 'MessageAttributes' not found at root nor in 'Body'", extra={"messageid": self.messageid})
+        return messageattributes
+
+    @property
+    def timestamp(self) -> datetime:
+        timestamp = None
+        if "timestamp" in self.body:
+            timestamp = self.body.get("timestamp")
+            timestamp = timestamp[:timestamp.find(".")]
+            timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S").timestamp()
+            timestamp = datetime.fromtimestamp(timestamp/1000.0).strftime('%Y-%m-%d %H:%M:%S')
+        elif "Timestamp" in self.body:  # is this the same as self.body["Timestamp"]?
+            timestamp = self.body.get("Timestamp")
+            timestamp = timestamp[:timestamp.find(".")]
+            timestamp = datetime.strptime(timestamp, "%Y-%m-%dT%H:%M:%S").timestamp()
+            timestamp = datetime.fromtimestamp(timestamp/1000.0).strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            LOGGER.error("Field 'timestamp' not found in 'Body' nor 'Message'", extra={"messageid": self.messageid})
+        return timestamp
+
+    @property
+    def tenant(self) -> str:
+        tenant = None
+        if "tenant" in self.messageattributes:
+            tenant = self.messageattributes.get("tenant")
+            # some messages have "Value" - UploadRecordingEvent and others "StringValue" - RecordingEvent
+            # Something to check upon, maybe align w/ other teams
+            if "Value" in tenant:
+                tenant = tenant["Value"]
+            elif "StringValue" in tenant:
+                tenant = tenant["StringValue"]
+        else:
+            LOGGER.error("Field 'tenant' not found in 'MessageAttributes'", extra={"messageid": self.messageid})
+        return tenant
+
+    @property
+    def topicarn(self) -> str:
+        """TopicArn is very inconsistent, some messages have it, other have 'topic' instead which has different meaning."""
+        topicarn = ""
+        if "TopicArn" in self.body:
+            topicarn = self.body.get("TopicArn")
+            topicarn = topicarn.split(":")[-1]
+        else:
+            LOGGER.error("Field 'TopicArn' not found in 'Body'", extra={"messageid": self.messageid})
+        return topicarn
+
+    @abstractproperty
+    def deviceid(self) -> str:
+        pass
+
+
+class VideoMessage(Message):
+    def __init__(self, sqs_message: dict = None) -> None:
+        super().__init__(sqs_message)
+        self._VIDEO_RECORDING_TYPES = ["InteriorRecorder", "TrainingRecorder", "FrontRecorder"]
+
+    def video_recording_type(self):
+        """Identify the recording type of a particular upload message
+
+        Returns:
+            video_recording_type (str): The type of the video event: InteriorRecorder, TrainingRecorder, or FrontRecorder
+        """
+        video_recording_types = ["InteriorRecorder", "TrainingRecorder", "FrontRecorder"]
+
+        if self.streamname != "": 
+            type = self.streamname
+        elif self.recordingid != "":
+            type = self.recordingid
+
+        for video_recording_type in video_recording_types:
+            if video_recording_type in type:
+                return video_recording_type
+
+    def validate(self) -> bool:
+        """Runtime tests to determine if message contents are usable
+
+        Returns:
+            bool: True if valid, else False
+        """
+        if self.tenant in TENANT_BLACKLIST:
+            LOGGER.info(f"Tenant {self.tenant} is blacklisted, message deemed invalid for ingestion", extra={"messageid": self.messageid})
+            return False
+        elif not self.topicarn:
+            LOGGER.info(f"Topic could not be identified, message deemed invalid for ingestion", extra={"messageid": self.messageid})
+            return False
+        elif not self.topicarn.endswith("video-footage-events"):
+            LOGGER.info(f"Topic '{self.topicarn}' is not for video footage events, message deemed invalid for ingestion", extra={"messageid": self.messageid})
+            return False
+        elif self.streamname == "":
+            LOGGER.info("Could not find a stream name, message deemed invalid for ingestion", extra={"messageid": self.messageid})
+            return False
+        # All FrontRecorder data is to be ignored for now
+        elif self.video_recording_type() == 'FrontRecorder':
+            LOGGER.info("Found 'FrontRecorder' video recorder, message deemed invalid for ingestion", extra={"messageid": self.messageid})
+            return False
+        return True
+
+    @property
+    def streamname(self) -> str:
+        streamname = ""
+        if 'streamName' in self.message:
+            streamname = self.message.get("streamName")
+        else:
+            LOGGER.error("Field 'streamName' not found in 'Message'", extra={"messageid": self.messageid})
+        return streamname
+
+    @property
+    def recording_type(self) -> str:
+        for video_recording_type in self._VIDEO_RECORDING_TYPES:
+            if video_recording_type in self.streamname:
+                return video_recording_type
+        return ""
+
+    @property
+    def recordingid(self) -> str:
+        """Messages with topic 'prod-inputEventsTerraform' have a 'recording_id' field,
+        but the ones with 'dev-video-footage-events' have a 'recordingId'
+        """
+        recordingid = ""
+        if self.topicarn == 'prod-inputEventsTerraform':
+            try:
+                recordingid = self.message["value"]["properties"]["recording_id"]
+            except:
+                LOGGER.error("Field 'recording_id' not found in 'properties'", extra={"messageid": self.messageid})
+
+        else:
+            if 'recordingId' in self.messageattributes:
+                try:
+                    recordingid = self.messageattributes["recordingId"]["Value"]
+                except:
+                    LOGGER.error("Field 'recordingId' not found in 'MessageAttributes'", extra={"messageid": self.messageid})
+        return recordingid
+
+    @property
+    def footagefrom(self) -> int:
+        footagefrom = 0
+        if 'footageFrom' in self.message:
+            footagefrom = self.message.get("footageFrom")
+            #footagefrom = datetime.fromtimestamp(footagefrom/1000.0)#, pytz.timezone('Europe/Berlin'))#.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            LOGGER.error("Field 'footageFrom' not found in 'Message'", extra={"messageid": self.messageid})
+        return footagefrom
+
+    @property
+    def footageto(self) -> int:
+        footageto = 0
+        if 'footageTo' in self.message:
+            footageto = self.message.get("footageTo")
+            #footageto = datetime.fromtimestamp(footageto/1000.0)#, pytz.timezone('Europe/Berlin'))#.strftime('%Y-%m-%d %H:%M:%S')
+        else:
+            LOGGER.error("Field 'footageTo' not found in 'Message'", extra={"messageid": self.messageid})
+        return footageto
+
+    @property
+    def uploadstarted(self) -> DT:
+        uploadstarted = None
+        if 'uploadStarted' in self.message:
+            uploadstarted = self.message.get("uploadStarted")
+            uploadstarted = datetime.fromtimestamp(uploadstarted/1000.0)
+        else:
+            LOGGER.error("Field 'uploadStarted' not found in 'Message'", extra={"messageid": self.messageid})
+        return uploadstarted
+
+    @property
+    def uploadfinished(self) -> DT:
+        uploadfinished = None
+        if 'uploadFinished' in self.message:
+            uploadfinished = self.message.get("uploadFinished")
+            uploadfinished = datetime.fromtimestamp(uploadfinished/1000.0)
+        else:
+            LOGGER.error("Field 'uploadFinished' not found in 'Message'", extra={"messageid": self.messageid})
+        return uploadfinished
+
+    @property
+    def deviceid(self) -> str:
+        if 'deviceId' in self.messageattributes:
+            deviceid = self.messageattributes.get("deviceId")['Value']
+        else:
+            LOGGER.error("Field 'deviceId' not found in 'MessageAttributes'", extra={"messageid": self.messageid})
+            return
+        return deviceid
+
+
+class SnapshotMessage(Message):
+    def __init__(self, sqs_message: dict = None) -> None:
+        super().__init__(sqs_message)
+
+    def validate(self) -> bool:
+        """Runtime tests to determine if message contents are usable
+
+        Returns:
+            bool: True if valid, else False
+        """
+        if self.tenant in TENANT_BLACKLIST:
+            LOGGER.info(f"Tenant {self.tenant} is blacklisted, message deemed invalid for ingestion", extra={"messageid": self.messageid})
+            return False
+        return True
+
+
+    @property
+    def chunks(self) -> list[Chunk]:
+        chunks = []
+        if 'chunk_descriptions' in self.properties:
+            chunks = self.properties.get('chunk_descriptions')
+            chunks = [Chunk(chunk_description) for chunk_description in chunks]
+            if chunks == []:
+                LOGGER.warning("Field 'chunk_descriptions' is empty", extra={"messageid": self.messageid})
+        else:
+            LOGGER.error("Field 'chunk_descriptions' not found in 'properties'", extra={"messageid": self.messageid})
+        return chunks
+
+    @property
+    def senttimestamp(self) -> DT:
+        senttimestamp = None
+        if 'SentTimestamp' in self.attributes:
+            senttimestamp = self.attributes.get('SentTimestamp')
+            senttimestamp = datetime.fromtimestamp(int(senttimestamp)/1000.0)
+        else:
+            LOGGER.error("Field 'SentTimestamp' not found in 'Attributes'", extra={"messageid": self.messageid})
+        return senttimestamp
+
+    @property
+    def eventtype(self) -> str:
+        if "eventType" in self.messageattributes:
+            eventtype = self.messageattributes.get("eventType")
+            # some messages have "Value" - UploadRecordingEvent and others "StringValue" - RecordingEvent
+            # Something to check upon, maybe align w/ other teams
+            if "Value" in eventtype:
+                eventtype = eventtype["Value"]
+            elif "StringValue" in eventtype:
+                eventtype = eventtype["StringValue"]
+            eventtype = eventtype.split('.')[-1]
+        else:
+            LOGGER.error("Field 'eventtype' not found in 'MessageAttributes'", extra={"messageid": self.messageid})
+            return
+        return eventtype
+
+    @property
+    def deviceid(self) -> str:
+        if 'device_id' in self.header:
+            device_id = self.header.get("device_id").split(":")[-1]
+        else:
+            LOGGER.error("Field 'device_id' not found in 'header'", extra={"messageid": self.messageid})
+            return
+        return device_id
+
+    @property
+    def header(self) -> dict:
+        header = {}
+        if 'header' in self.properties:
+            header = self.properties.get("header")
+        else:
+            LOGGER.error("Field 'header' not found in 'properties'", extra={"messageid": self.messageid})
+        return header
+
+    @property
+    def properties(self) -> dict:
+        properties = {}
+        if 'properties' in self.value:
+            properties = self.value.get("properties")
+        else:
+            LOGGER.error("Field 'properties' not found in 'value'", extra={"messageid": self.messageid})
+        return properties
+
+    @property
+    def value(self) -> dict:
+        value = {}
+        if 'value' in self.body:
+            value = self.body.get("value")
+        elif 'value' in self.message:
+            value = self.message.get("value")
+        else:
+            LOGGER.error("Field 'value' not found in 'Message' nor 'Body'", extra={"messageid": self.messageid})
+        return value
+
+"""Metadata is not received as message, so we don't have a class to represent it.
+Instead, we assume it exists on RCC at the moment we get a notification for its video."""
---
+++ SDRetriever/src/tests/test_videoingestor.py
@@ -0,0 +1,60 @@
+import pytest
+from unittest import mock
+from unittest.mock import MagicMock, ANY
+from ingestor import VideoIngestor
+from datetime import datetime
+import os
+
+@pytest.mark.usefixtures("container_services", "s3_client", "sqs_client", "sts_helper", "msg_interior")
+class TestVideoIngestor():
+    
+    
+
+    @mock.patch("ingestor.subprocess.run")
+    def test_ffmpeg_probe_video(self, mock_run, video_bytes, container_services, s3_client, sqs_client, sts_helper):
+        obj = VideoIngestor(container_services, s3_client, sqs_client, sts_helper)
+        mock_stdout = MagicMock()
+        mock_stdout.configure_mock(
+            **{"stdout.decode.return_value": '{"something":"something else"}'.encode("utf-8")}
+        )
+
+        mock_run.return_value = mock_stdout
+        result = obj._ffmpeg_probe_video(video_bytes)
+        assert result == {"something":"something else"}
+    
+    @mock.patch("ingestor.subprocess.run")
+    def test_ingest(self, mock_run, container_services, s3_client, sqs_client, sts_helper, msg_interior, video_bytes):
+        mock_stdout = MagicMock()
+        mock_stdout.configure_mock(
+            **{"stdout.decode.return_value": '{"streams":[{"width":1920,"height":1080}],"format":{"duration":1000}}'.encode("utf-8")}
+        )
+
+        mock_run.return_value = mock_stdout
+        obj = VideoIngestor(container_services, s3_client, sqs_client, sts_helper)
+
+        db_record_data, request_metadata = obj.ingest(msg_interior)
+        os.remove("input_video.mp4")
+        
+        expected_raw_path = "Debug_Lync/datanauts_DATANAUTS_DEV_01_InteriorRecorder_1657297040802_1657297074110.mp4"
+        expected_db_record_data = {
+            "_id": "datanauts_DATANAUTS_DEV_01_InteriorRecorder_1657297040802_1657297074110",
+            "s3_path": container_services.raw_s3 + "/" + expected_raw_path,
+            "recording_overview": {
+                'length': "0:16:40",
+                'resolution': "1920x1080",
+                '#snapshots': "0",
+                'snapshots_paths': {},
+                },
+            "MDF_available": "No",
+            "sync_file_ext": "",
+            "tenant":"datanauts",
+            "deviceid":"DATANAUTS_DEV_01",
+            "footagefrom":1657297040802,
+            "footageto":1657297074110
+        }
+        video_from = datetime.fromtimestamp(msg_interior.footagefrom/1000.0).strftime('%Y-%m-%d %H:%M:%S')
+        video_to = datetime.fromtimestamp(msg_interior.footageto/1000.0).strftime('%Y-%m-%d %H:%M:%S')
+        container_services.get_kinesis_clip.assert_called_once_with(ANY,"datanauts_DATANAUTS_DEV_01_InteriorRecorder",video_from,video_to,"PRODUCER_TIMESTAMP")
+        container_services.upload_file.assert_called_once_with(ANY,ANY,container_services.raw_s3,expected_raw_path)
+        assert db_record_data == expected_db_record_data
+        assert request_metadata == True
