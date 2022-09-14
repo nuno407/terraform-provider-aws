ef0f392b55e5d1c68234513a2d45cc2859c3e1d4
Michael Krebs <michael.krebs@bosch.io>
2022-09-14 11:36:34 +0200

    Hotfix: specify config file as absolute path

--- MDFParser/src/main.py
+++ MDFParser/src/main.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 import re
 from typing import Any, TypedDict, cast
 import boto3
@@ -21,11 +22,12 @@ _logger: logging.Logger
 def main():
     # External configuration that can be configured as kubernetes secret
     config: MdfParserConfig
-    with open('../config/config.yml', 'r') as configfile:
+    with open(os.environ.get('CONFIG_FILE', '/app/config/config/yml'), 'r') as configfile:
         config = cast(MdfParserConfig, yaml.load(configfile, yaml.SafeLoader))
 
-    _logger.info("Starting Container %s (%s)..\n", CONTAINER_NAME,CONTAINER_VERSION)
-    
+    _logger.info("Starting Container %s (%s)..\n",
+                 CONTAINER_NAME, CONTAINER_VERSION)
+
     # Logic classes
     downloader = Downloader()
     uploader = Uploader()
@@ -34,34 +36,42 @@ def main():
     processors: list[Processor] = [chc_counter]
 
     # AWS clients for container_services
-    sqs_client = boto3.client('sqs',region_name='eu-central-1')
-    container_services = ContainerServices(container=CONTAINER_NAME,version=CONTAINER_VERSION)
+    sqs_client = boto3.client('sqs', region_name='eu-central-1')
+    container_services = ContainerServices(
+        container=CONTAINER_NAME, version=CONTAINER_VERSION)
 
     # allow graceful exit
     graceful_exit = GracefulExit()
 
     while(graceful_exit.continue_running):
         _logger.debug('Listening to input queue.')
-        message = container_services.listen_to_input_queue(sqs_client, config['input_queue'])
+        message = container_services.listen_to_input_queue(
+            sqs_client, config['input_queue'])
         if message:
             # check message has the required fields
             if not('Body' in message and '_id' in message['Body'] and 's3_path' in message['Body']):
                 _logger.warning('Required fields are not in the message.')
                 continue
 
-            message_body = cast(InputMessage, json.loads(message['Body'].replace("\'", "\"")))
+            message_body = cast(InputMessage, json.loads(
+                message['Body'].replace("\'", "\"")))
             _logger.debug('Processing recording entry %s', message_body['_id'])
             try:
                 metadata: dict[str, Any] = {
                     '_id': message_body['_id'],
                 }
-                metadata.update(process_request(message_body['s3_path'], downloader, uploader, synchronizer, processors))
-                container_services.send_message(sqs_client, config['metadata_output_queue'], metadata)
-                container_services.delete_message(sqs_client, message['ReceiptHandle'], config['input_queue'])
+                metadata.update(process_request(
+                    message_body['s3_path'], downloader, uploader, synchronizer, processors))
+                container_services.send_message(
+                    sqs_client, config['metadata_output_queue'], metadata)
+                container_services.delete_message(
+                    sqs_client, message['ReceiptHandle'], config['input_queue'])
             except Exception:
-                _logger.exception('Error during processing of request for %s', message_body['_id'])
+                _logger.exception(
+                    'Error during processing of request for %s', message_body['_id'])
 
-def process_request(mdf_s3_path: str, downloader: Downloader, uploader: Uploader, synchronizer: Synchronizer, processors: list[Processor])->dict[str, Any]:
+
+def process_request(mdf_s3_path: str, downloader: Downloader, uploader: Uploader, synchronizer: Synchronizer, processors: list[Processor]) -> dict[str, Any]:
     _logger.info('Starting processing of metadata for %s', mdf_s3_path)
 
     # download and synchronize metadata
@@ -82,40 +92,55 @@ def process_request(mdf_s3_path: str, downloader: Downloader, uploader: Uploader
             # we do not want the entire recording to fail for a specific processing only
             _logger.exception('Error processing metadata.')
     if(successful_processings == 0):
-        raise NoProcessingSuccessfulException('Not a single processing succeeded, therefore not updating metadata.')
+        raise NoProcessingSuccessfulException(
+            'Not a single processing succeeded, therefore not updating metadata.')
 
     _logger.info('Successfully processed metadata for %s', mdf_s3_path)
 
     # upload synchronized signals to s3 and store path in metadata
     try:
-        metadata['signals_file'] = uploader.upload_signals(synchronized, mdf_s3_path)
-        metadata["s3_path"] = metadata['signals_file']['bucket']+"/"+metadata['signals_file']["key"] # added because Metadata expects this field to exist
+        metadata['signals_file'] = uploader.upload_signals(
+            synchronized, mdf_s3_path)
+        # added because Metadata expects this field to exist
+        metadata["s3_path"] = metadata['signals_file']['bucket'] + \
+            "/"+metadata['signals_file']["key"]
     except Exception:
         _logger.exception('Error uploading synchronized signals to S3.')
         raise
 
     return metadata
-        
-def extract_timestamps(filepath: str)->tuple[int, int]:
+
+
+def extract_timestamps(filepath: str) -> tuple[int, int]:
     match = re.search(r"_(\d{13,})_(\d{13,})_", filepath)
-    if not match or len(match.groups())<2:
-        raise InvalidFileNameException('Cannot extract timestamps from filepath "' + filepath + '".')
+    if not match or len(match.groups()) < 2:
+        raise InvalidFileNameException(
+            'Cannot extract timestamps from filepath "' + filepath + '".')
     timestamp_from = int(match.group(1))
     timestamp_to = int(match.group(2))
     return timestamp_from, timestamp_to
+
+
 class InvalidFileNameException(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+
 class NoProcessingSuccessfulException(Exception):
     def __init__(self, *args: object) -> None:
         super().__init__(*args)
+
+
 class MdfParserConfig(TypedDict):
     input_queue: str
     metadata_output_queue: str
     log_level: str
+
+
 class InputMessage(TypedDict):
     _id: str
     s3_path: str
+
 
 if __name__ == '__main__':
     # Define configuration for logging messages
--- MDFParser/src/tests/integration/test_mdfparser.py
+++ MDFParser/src/tests/integration/test_mdfparser.py
@@ -5,32 +5,37 @@ from unittest.mock import ANY, Mock, PropertyMock, patch
 
 from pytest import LogCaptureFixture, fixture
 from pytest_mock import MockerFixture
-import main
+import mdfparser.main
 
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 recording_from = 1659962815000
 recording_to = 1659962819000
-recording_name = 'tenant_recording_' + str(recording_from) + '_' + str(recording_to)
+recording_name = 'tenant_recording_' + \
+    str(recording_from) + '_' + str(recording_to)
 s3_path = 's3://bucket/' + recording_name + '_metadata_full.json'
+
 
 class TestMain:
     @fixture
     def boto3_mock(self, mocker: MockerFixture):
-        mock = mocker.patch('main.boto3')
-        return mocker.patch('s3_interaction.boto3')
+        mock = mocker.patch('mdfparser.main.boto3')
+        return mocker.patch('mdfparser.s3_interaction.boto3')
 
     @fixture
     def container_services_mock(self, mocker: MockerFixture, boto3_mock: Mock) -> Mock:
-        container_services_mock = mocker.patch('main.ContainerServices', autospec=True)
-        mocker.patch('downloader.ContainerServices', container_services_mock)
-        mocker.patch('uploader.ContainerServices', container_services_mock)
+        container_services_mock = mocker.patch(
+            'mdfparser.main.ContainerServices', autospec=True)
+        mocker.patch('mdfparser.downloader.ContainerServices',
+                     container_services_mock)
+        mocker.patch('mdfparser.uploader.ContainerServices',
+                     container_services_mock)
         return container_services_mock
 
     @fixture
     def graceful_exit_mock(self, mocker: MockerFixture) -> Mock:
-        graceful_exit_mock = mocker.patch('main.GracefulExit')
+        graceful_exit_mock = mocker.patch('mdfparser.main.GracefulExit')
         continue_running_mock = PropertyMock(side_effect=[True, False])
         type(graceful_exit_mock.return_value).continue_running = continue_running_mock
         return continue_running_mock
@@ -55,17 +60,21 @@ class TestMain:
         container_services_mock.download_file.return_value = mdf_data_encoded
 
         ### WHEN ###
-        main.main()
+        mdfparser.main.main()
 
         ### THEN ###
         with open(os.path.join(__location__, 'test_data/recording_update_expected.json'), 'r') as f:
             expected_update = json.loads(f.read())
         with open(os.path.join(__location__, 'test_data/sync_expected.json'), 'r') as f:
-            expected_sync = json.dumps(json.loads(f.read().encode('utf-8'))).encode('utf-8')
-        
-        container_services_mock.return_value.send_message.assert_called_once_with(ANY, 'dev-terraform-queue-metadata', expected_update)
-        container_services_mock.return_value.delete_message.assert_called_once_with(ANY, message['ReceiptHandle'], 'dev-terraform-queue-mdf-parser')
-        container_services_mock.upload_file.assert_called_once_with(ANY, expected_sync, 'bucket', recording_name + '_signals.json')
+            expected_sync = json.dumps(json.loads(
+                f.read().encode('utf-8'))).encode('utf-8')
+
+        container_services_mock.return_value.send_message.assert_called_once_with(
+            ANY, 'dev-terraform-queue-metadata', expected_update)
+        container_services_mock.return_value.delete_message.assert_called_once_with(
+            ANY, message['ReceiptHandle'], 'dev-terraform-queue-mdf-parser')
+        container_services_mock.upload_file.assert_called_once_with(
+            ANY, expected_sync, 'bucket', recording_name + '_signals.json')
         assert(graceful_exit_mock.call_count == 2)
 
     def test_mdf_parsing_invalid_path(self, mocker: MockerFixture, container_services_mock: Mock, graceful_exit_mock, caplog: LogCaptureFixture):
@@ -82,10 +91,11 @@ class TestMain:
         container_services_mock.return_value.listen_to_input_queue.return_value = message
 
         ### WHEN ###
-        main.main()
+        mdfparser.main.main()
 
         ### THEN ###
-        assert('Invalid MDF path' in rec.message for rec in caplog.records if rec.levelname == 'ERROR')
+        assert(
+            'Invalid MDF path' in rec.message for rec in caplog.records if rec.levelname == 'ERROR')
         container_services_mock.return_value.send_message.assert_not_called()
         container_services_mock.return_value.delete_message.assert_not_called()
         container_services_mock.upload_file.assert_not_called()
@@ -104,13 +114,15 @@ class TestMain:
         # input queue mocks
         container_services_mock.return_value.listen_to_input_queue.return_value = message
         # downloader mocks
-        container_services_mock.download_file.side_effect = Exception('Download error')
+        container_services_mock.download_file.side_effect = Exception(
+            'Download error')
 
         ### WHEN ###
-        main.main()
+        mdfparser.main.main()
 
         ### THEN ###
-        assert('Download error' in rec.message for rec in caplog.records if rec.levelname == 'ERROR')
+        assert(
+            'Download error' in rec.message for rec in caplog.records if rec.levelname == 'ERROR')
         container_services_mock.return_value.send_message.assert_not_called()
         container_services_mock.return_value.delete_message.assert_not_called()
         container_services_mock.upload_file.assert_not_called()
