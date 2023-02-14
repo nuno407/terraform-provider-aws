9b9ec38f6dac0211649273f98a31411e3a836888
Ricardo Martins <ricardo.martins3@bosch.com>
2023-02-14 15:26:36 +0000

    MC-37207: decouple data importer from s3 configuration loading

--- DataImporter/dataimporter/main.py
+++ DataImporter/dataimporter/main.py
@@ -2,11 +2,7 @@
 import json
 import boto3
 import os
-from typing import Optional
-from dataclasses import dataclass
 from base.aws.container_services import ContainerServices
-from base.constants import IMAGE_FORMATS, VIDEO_FORMATS
-from pathlib import Path
 
 CONTAINER_NAME = "DataImporter" # Name of the current container
 CONTAINER_VERSION = "v1.0"      # Version of the current container
@@ -21,21 +17,18 @@ def main(stop_condition=lambda: True):
     _logger.info("Starting Container %s (%s)..\n", CONTAINER_NAME, CONTAINER_VERSION)
 
     # Create the necessary clients for AWS services access
-    s3_client = boto3.client('s3', region_name='eu-central-1')
     sqs_client = boto3.client('sqs', region_name='eu-central-1')
 
     # Initialise instance of ContainerServices class
     container_services = ContainerServices(container=CONTAINER_NAME, version=CONTAINER_VERSION)
 
     # Load global variable values from config json file (S3 bucket)
-    container_services.load_config_vars(s3_client)
-    container_services.input_queue = DATA_IMPORTER_QUEUE
 
     _logger.info("\nListening to input queue(s)..\n\n")
 
     while stop_condition():
         # Check input SQS queue for new messages
-        sqs_messages = container_services.get_multiple_messages_from_input_queue(sqs_client, max_number_of_messages = 100)
+        sqs_messages = container_services.get_multiple_messages_from_input_queue(sqs_client, input_queue=DATA_IMPORTER_QUEUE, max_number_of_messages = 100)
 
         _logger.info("Batch received!")
         for message in sqs_messages:
@@ -46,7 +39,7 @@ def main(stop_condition=lambda: True):
             # processing_sdm(container_services, sqs_client, sqs_message)
 
             # Delete message after processing
-            container_services.delete_message(sqs_client, message['ReceiptHandle'])
+            container_services.delete_message(sqs_client, message['ReceiptHandle'], input_queue=DATA_IMPORTER_QUEUE)
 
 
 if __name__ == '__main__':
