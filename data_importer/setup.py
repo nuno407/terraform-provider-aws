e273a162f28dd1e84a50242fe89e729f45cfc520
Marian Bäuerle <marian.baeuerle@bosch.io>
2023-02-21 12:50:52 +0100

    MC-37512 Fix code coverage paths

--- data_importer/setup.py
+++ data_importer/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name="dataimporter",
+setup(name="data_importer",
       version="1.0",
       # list folders, not files
       packages=["data_importer"]
