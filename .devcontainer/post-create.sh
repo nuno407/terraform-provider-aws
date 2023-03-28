a42ef3f292ed41887cd379190596681aa8956e0e
Michael Krebs <michael.krebs@bosch.io>
2023-03-28 07:26:57 +0000

    Add devcontainer configuration

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,14 @@
+## CaskaydiaCode Nerd Font
+mkdir -p $HOME/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/CascadiaCode.zip
+unzip CascadiaCode.zip -d $HOME/.local/share/fonts
+rm CascadiaCode.zip
+
+## OH-MY-ZSH PLUGINS & THEMES (POWERLEVEL10K) ##
+# install oh-my-zsh plugins and themes (powerlevel10k) without dotfiles integration
+git clone https://github.com/zsh-users/zsh-completions.git $HOME/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
