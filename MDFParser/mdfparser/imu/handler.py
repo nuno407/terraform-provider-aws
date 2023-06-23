7e06e65e73b7a844a2020e460c8af535fffeaf02
Miguel <miguel.martins3@bosch.com>
2023-06-23 12:26:27 +0100

    MC-40850: Log MDFParser memory usage

--- MDFParser/mdfparser/imu/handler.py
+++ MDFParser/mdfparser/imu/handler.py
@@ -9,6 +9,8 @@ from mdfparser.imu.uploader import IMUUploader
 from mdfparser.interfaces.handler import Handler
 from mdfparser.interfaces.input_message import DataType, InputMessage
 from mdfparser.interfaces.output_message import OutputMessage
+import logging
+_logger = logging.getLogger("mdfparser." + __name__)
 
 
 @inject
@@ -37,6 +39,19 @@ class IMUHandler(Handler):
         self.transformer = transformer
         self.config = config
 
+    @staticmethod
+    def get_megabyte_memory_usage(df: pd.DataFrame) -> float:
+        """
+        Retrieves the memory used by a dataframe in megabytes
+
+        Args:
+            df (pd.DataFrame): The dataframe
+
+        Returns:
+            float: Megabytes
+        """
+        return df.memory_usage(index=True).sum() / (1024 * 1024)
+
     def ingest(self, message: InputMessage) -> OutputMessage:
         """
         Process the IMU data.
@@ -50,15 +65,19 @@ class IMUHandler(Handler):
 
         # Download the IMU data
         imu_data: pd.DataFrame = self.downloader.download(message.s3_path)
+        _logger.info("IMU has been downloaded and parsed with size=%fMB", self.get_megabyte_memory_usage(imu_data))
 
         # Apply necessary transformations
         processed_imu: pd.DataFrame = self.transformer.apply_transformation(imu_data)
+        _logger.debug("IMU transformation has been applied reducing size to %f MB",
+                      self.get_megabyte_memory_usage(processed_imu))
 
         # Free memory
         del imu_data
 
         # Apply source information to every field
         self.__apply_source_info(processed_imu, message)
+        _logger.info("Final IMU dataframe size=%fMB", self.get_megabyte_memory_usage(processed_imu))
 
         # Upload the file
         tmp_file_key = f"{message.id}{IMU_TMP_SUFFIX}"
