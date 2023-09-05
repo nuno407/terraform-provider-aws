bf19c8c4d90adb20bf5e7730e4e6be5c6f750a66
Marian Bäuerle <marian.baeuerle@de.bosch.com>
2023-09-05 17:37:24 +0200

    NO-TICKET: Fix issues on Mac platforms with PicklingError
    The full error is:
    "_pickle.PicklingError: Can't pickle <class 'botocore.client.SQS'>: attribute lookup SQS on botocore.client failed"
    
    This issue arises as multiprocessing.Process uses different mechanisms on Mac/Win (spawn) and Linux (fork) to spawn a process.
    See: https://docs.python.org/3/library/multiprocessing.html#contexts-and-start-methods
    
    The fix is to switch to Threads and inform the thread about termination directly.

--- base/base/aws/auto_message_visibility_increaser.py
+++ base/base/aws/auto_message_visibility_increaser.py
@@ -1,6 +1,5 @@
 """ Module for auto MessageVisibility Increaser. """
-import multiprocessing
-import time
+import threading
 from base.aws.container_services import ContainerServices
 
 _logger = ContainerServices.configure_logging(__name__)
@@ -18,29 +17,33 @@ class AutoMessageVisibilityIncreaser:
     """
 
     def _increase_visibility_timeout(self):
-        while True:
-            time.sleep(self.interval)
+        while not self._stop_event.wait(timeout=self.interval):
             _logger.info("Increasing visibility timeout.")
             self.container_services.update_message_visibility(
                 self.sqs_client, self.receipt_handle, self.interval + 30, input_queue=self.input_queue)
+
+    def stop(self):
+        """Stop the _increase_visibility_timeout thread gracefully."""
+        self._stop_event.set()
 
     def __init__(self, sqs_client,  # pylint: disable=too-many-arguments
                  receipt_handle: str,
                  container_services: ContainerServices,
                  interval: float,
                  input_queue: str):
+        self._stop_event = threading.Event()
         self.sqs_client = sqs_client
         self.receipt_handle = receipt_handle
         self.container_services = container_services
         self.interval = interval
         self.input_queue = input_queue
-        self.visibility_process = multiprocessing.Process(
+        self.visibility_process = threading.Thread(
             target=AutoMessageVisibilityIncreaser._increase_visibility_timeout, args=[self])
 
     def __enter__(self):
         self.visibility_process.start()
 
     def __exit__(self, _type, _value, _traceback):
-        self.visibility_process.kill()
+        self.stop()
         self.visibility_process.join()
-        _logger.info("AutoMessageVisibilityIncreaser process killed")
+        _logger.debug("AutoMessageVisibilityIncreaser process shut down")
--- base/tests/unit/test_auto_message_visibility_increaser.py
+++ base/tests/unit/test_auto_message_visibility_increaser.py
@@ -8,38 +8,34 @@ from base.aws.auto_message_visibility_increaser import AutoMessageVisibilityIncr
 class TestAutoMessageVisibilityIncreaser():
     """Test AutoMessageVisibilityIncreaser."""
 
-    @patch("base.aws.auto_message_visibility_increaser.time.sleep")
-    @patch("base.aws.auto_message_visibility_increaser.multiprocessing.Process")
-    def test_happy_path(self, multiprocessing_process_mock, _):
+    @patch("base.aws.auto_message_visibility_increaser.threading.Thread")
+    def test_happy_path(self, multiprocessing_process_mock):
         sqs_client_mock = Mock()
         container_services_mock = Mock()
         multiprocessing_process_mock.return_value.start = Mock()
-        multiprocessing_process_mock.return_value.kill = Mock()
         multiprocessing_process_mock.return_value.join = Mock()
 
-        with AutoMessageVisibilityIncreaser(sqs_client_mock, "Receipt_handle", container_services_mock, 0, "input_queue"):
+        with AutoMessageVisibilityIncreaser(sqs_client_mock, "Receipt_handle",
+                                            container_services_mock, 0, "input_queue"):
             pass
 
         multiprocessing_process_mock.assert_called_once()
         multiprocessing_process_mock.return_value.start.assert_called_once()
-        multiprocessing_process_mock.return_value.kill.assert_called_once()
         multiprocessing_process_mock.return_value.join.assert_called_once()
 
-    @patch("base.aws.auto_message_visibility_increaser.time.sleep")
-    @patch("base.aws.auto_message_visibility_increaser.multiprocessing.Process")
-    def test_unhappy_path(self, multiprocessing_process_mock, _):
+    @patch("base.aws.auto_message_visibility_increaser.threading.Thread")
+    def test_unhappy_path(self, multiprocessing_process_mock):
         sqs_client_mock = Mock()
         container_services_mock = Mock()
         multiprocessing_process_mock.return_value.start = Mock()
-        multiprocessing_process_mock.return_value.kill = Mock()
         multiprocessing_process_mock.return_value.join = Mock()
 
         try:
-            with AutoMessageVisibilityIncreaser(sqs_client_mock, "Receipt_handle", container_services_mock, 0, "input_queue"):
+            with AutoMessageVisibilityIncreaser(sqs_client_mock, "Receipt_handle",
+                                                container_services_mock, 0, "input_queue"):
                 raise Exception("Test")
         except Exception:
             pass
 
         multiprocessing_process_mock.assert_called_once()
-        multiprocessing_process_mock.return_value.start.assert_called_once()
         multiprocessing_process_mock.return_value.join.assert_called_once()
