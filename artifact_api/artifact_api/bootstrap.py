695098b9d584707016dcc18b3d0daeaa86aa5e36
xekster <ricardo.martins3@bosch.com>
2023-10-27 13:13:54 +0100

    Fix configmap names on artifact api bootstrap

--- artifact_api/artifact_api/bootstrap.py
+++ artifact_api/artifact_api/bootstrap.py
@@ -10,8 +10,8 @@ def bootstrap():
     """Intilize dependency injection"""
     ContainerServices.configure_logging("artifact_api")
 
-    di["config_path"] = os.getenv("ARTIFACT_API_CONFIG_PATH", "/app/config/config.yml")
-    di["tenant_config_path"] = os.getenv("TENANT_MAPPING_CONFIG_PATH", "/app/tenant-mapping-conf/tenant-mapping-conf.yml")
+    di["config_path"] = os.getenv("ARTIFACT_API_CONFIG_PATH", "/app/config/config.yaml")
+    di["tenant_config_path"] = os.getenv("TENANT_MAPPING_CONFIG_PATH", "/app/tenant-mapping-conf/tenant-mapping-conf.yaml")
     di["mongodb_config_path"] = os.environ.get("MONGODB_CONFIG", "/app/mongo-conf/mongo_config.yaml")
     di["container_name"] = os.getenv("CONTAINER_NAME", "ArtifactAPI")
     di[VoxelConfig] = VoxelConfig.load_yaml_config(di["tenant_config_path"])
