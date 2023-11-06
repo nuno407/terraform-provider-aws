44c47d1faa03dd54b58f242a31d48c417d23c368
Brw4hi <114382237+Brw4hi@users.noreply.github.com>
2023-11-06 11:34:21 +0100

    MC-47075 update person count variance rule (#741)
    update person count variance rule
    
    Co-authored-by: brw4hi <uwe.brosch@bosch.com>

--- Selector/selector/rules/high_person_count_variance.py
+++ Selector/selector/rules/high_person_count_variance.py
@@ -5,22 +5,23 @@ from base.model.artifacts import RecorderType
 from selector.context import Context
 from selector.decision import Decision
 from selector.model import PreviewMetadata
-from selector.rule import Rule
+from selector.rules.basic_rule import BaseRule
 
 logger = logging.getLogger(__name__)
 
 
-class HighPersonCountVarianceRule(Rule):
-    """Rule that uploads training data if PersonCount_value variance is bigger than 1"""
-    @property
-    def attribute_name(self) -> str:
-        """attribute name"""
-        return "PersonCount_value"
+class HighPersonCountVarianceRule(BaseRule):
+    """Rule that uploads training data if PersonCount_value variance is bigger than 1.5"""
 
-    @property
-    def rule_name(self) -> str:
-        """rule name"""
-        return "High Person Count Variance"
+    def __init__(  # pylint: disable=too-many-arguments
+            self,
+            attribute_name: str = "PersonCount_value",
+            rule_name: str = "High Person Count Variance",
+            min_ride_length_in_minutes: int = 1,
+
+    ) -> None:
+        super().__init__(attribute_name=attribute_name, rule_name=rule_name)
+        self._min_ride_length_in_minutes = min_ride_length_in_minutes
 
     def evaluate(self, context: Context) -> list[Decision]:
         logger.debug("Evaluating '%s' rule", self.rule_name)
@@ -43,10 +44,10 @@ class HighPersonCountVarianceRule(Rule):
 
     def _has_high_person_count_variance(self, metadata: PreviewMetadata) -> bool:
         person_count = [int(i.value) for i in metadata.get_integer(self.attribute_name) if i.value is not None]
-        if len(person_count) == 0:
+        if len(person_count) <= self._min_ride_length_in_minutes * 60:
             variance = float(0)
         else:
             mean = sum(person_count) / len(person_count)
             variance = sum((xi - mean) ** 2 for xi in person_count) / len(person_count)
             variance = round(variance, 2)
-        return variance > 1
+        return variance > 1.5
--- Selector/tests/unit/test_rule_high_person_count_variance.py
+++ Selector/tests/unit/test_rule_high_person_count_variance.py
@@ -11,6 +11,7 @@ from selector.context import Context
 from selector.model import PreviewMetadataV063
 from selector.rule import Rule
 from selector.rules import HighPersonCountVarianceRule
+from .utils import DataTestBuilder
 
 tenant_device_and_timing = {
     "tenant_id": "tenant_id",
@@ -26,6 +27,32 @@ class TestRuleHighPersonCount:
     @property
     def _attribute_name(self) -> str:
         return "PersonCount_value"
+
+    @fixture
+    def data_builder(self) -> DataTestBuilder:
+        return DataTestBuilder().with_length(15 * 60).with_frames_per_sec(1)
+
+    @fixture
+    def data_person_count_one(self, data_builder: DataTestBuilder) -> PreviewMetadataV063:
+        return data_builder.with_integer_attribute(self._attribute_name, 1).build()
+
+    @fixture
+    def data_person_count_high_variance(self, data_builder: DataTestBuilder) -> PreviewMetadataV063:
+        data = data_builder.with_integer_attribute(self._attribute_name, 1).build()
+        pc_value = 0
+        for frame in data.frames:
+            for object in frame.objectlist:
+                if isinstance(object, IntegerObject) and self._attribute_name in object.integer_attributes:
+                    object.integer_attributes[self._attribute_name] = str(pc_value)  # type: ignore
+                    pc_value += 2
+                    if pc_value == 6:
+                        pc_value = 0
+        return data
+
+    @fixture
+    def too_short_data(self, data_builder: DataTestBuilder) -> PreviewMetadataV063:
+        return data_builder.with_length(
+            4 * 60).with_integer_attribute(self._attribute_name, 1).build()
 
     @fixture
     def rule(self):
@@ -47,19 +74,11 @@ class TestRuleHighPersonCount:
         assert rule.rule_name == "High Person Count Variance"
 
     def test_positive_evaluation(self,
-                                 minimal_preview_metadata: PreviewMetadataV063,
+                                 data_person_count_high_variance: PreviewMetadataV063,
                                  rule: Rule,
                                  artifact: PreviewSignalsArtifact):
         # GIVEN
-        pc_value = 0
-        for frame in minimal_preview_metadata.frames:
-            for object in frame.objectlist:
-                if isinstance(
-                        object, IntegerObject) and self._attribute_name in object.integer_attributes:
-                    object.integer_attributes[self._attribute_name] = str(pc_value)  # type: ignore
-                    pc_value += 1
-        ctx = Context(minimal_preview_metadata, artifact)
-
+        ctx = Context(data_person_count_high_variance, artifact)
         # WHEN
         decisions = rule.evaluate(ctx)
         # THEN
@@ -67,19 +86,25 @@ class TestRuleHighPersonCount:
         assert recorders == {RecorderType.TRAINING}
 
     def test_negative_evaluation(self,
-                                 minimal_preview_metadata: PreviewMetadataV063,
+                                 data_person_count_one: PreviewMetadataV063,
                                  rule: Rule,
                                  artifact: PreviewSignalsArtifact):
         # GIVEN
-        for i, frame in enumerate(minimal_preview_metadata.frames):
-            for object in frame.objectlist:
-                if isinstance(object, IntegerObject):
-                    object.integer_attributes[self._attribute_name] = "1"  # type: ignore
-        ctx = Context(minimal_preview_metadata, artifact)
-
+        ctx = Context(data_person_count_one, artifact)
         # WHEN
         decisions = rule.evaluate(ctx)
+        # THEN
+        recorders = set(map(lambda d: d.recorder, decisions))
+        assert recorders == set()
 
+    def test_too_short_data_does_not_select_ride(self,
+                                                 rule: Rule,
+                                                 too_short_data: PreviewMetadataV063,
+                                                 artifact: PreviewSignalsArtifact):
+        # GIVEN
+        ctx = Context(too_short_data, artifact)
+        # WHEN
+        decisions = rule.evaluate(ctx)
         # THEN
         recorders = set(map(lambda d: d.recorder, decisions))
         assert recorders == set()
