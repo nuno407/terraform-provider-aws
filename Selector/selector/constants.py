73146638a464ede626ddd40cf80e4d61fd3cc229
Anselmo Gonçalves <103199037+AnselmoG@users.noreply.github.com>
2024-01-31 10:29:55 +0000

    NO-TICKET: Allow new metadata version (#820)
    * NO-TICKET: Allow new metadata version
    
    * NO-TICKET: Update metadata version to accept 2.X

--- Selector/selector/constants.py
+++ Selector/selector/constants.py
@@ -4,7 +4,7 @@ from base.model.artifacts import RecorderType
 CONTAINER_NAME = "Selector"    # Name of the current container
 CONTAINER_VERSION = "v2.0"      # Version of the current container
 
-ALLOWED_METADATA_VERSIONS = ["0.6", "2.3."]
+ALLOWED_METADATA_VERSIONS = ["0.6", "2."]
 # Maps the artifact recorder names to the ones used by the footage API
 FOOTAGE_RECORDER_NAME_MAP = {
     RecorderType.TRAINING: "TRAINING",
