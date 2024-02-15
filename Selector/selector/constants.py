977d10c6a5276773a50e64b7ba17be2df4c511ce
Nuno Sousa <nuno407@users.noreply.github.com>
2024-02-15 10:45:04 +0000

    MC-50133: Enable metadata version regex matching on metadata (#835)

--- Selector/selector/constants.py
+++ Selector/selector/constants.py
@@ -4,7 +4,10 @@ from base.model.artifacts import RecorderType
 CONTAINER_NAME = "Selector"    # Name of the current container
 CONTAINER_VERSION = "v2.0"      # Version of the current container
 
-ALLOWED_METADATA_VERSIONS = ["0.6", "2."]
+# The allowed metadata versions. Should follow a normal regex pattern from the re module
+# As of now, every version is allowed
+ALLOWED_METADATA_VERSIONS = [r"\d+"]
+
 # Maps the artifact recorder names to the ones used by the footage API
 FOOTAGE_RECORDER_NAME_MAP = {
     RecorderType.TRAINING: "TRAINING",
--- Selector/selector/model/preview_metadata_63.py
+++ Selector/selector/model/preview_metadata_63.py
@@ -2,12 +2,13 @@
 
 from datetime import datetime, timedelta
 from typing import Iterator, Optional
-
+import re
 from pydantic import field_validator, Field
+
 from base.model.metadata.base_metadata import Resolution, PtsTimeWindow, UtcTimeWindow, \
     BaseFrame, FrameSignal, Pose, ObjectList, StringObject, FloatObject, BoolObject, IntegerObject
 from selector.model.preview_metadata import PreviewMetadata
-from selector.constants import ALLOWED_METADATA_VERSIONS  # pylint: disable=unused-import
+from selector.constants import ALLOWED_METADATA_VERSIONS
 
 
 class Frame(BaseFrame):
@@ -67,9 +68,8 @@ class PreviewMetadataV063(PreviewMetadata):
         if not isinstance(version, str):
             raise ValueError("Metadata does not contain a version")
 
-        # if not any(map(version.startswith, ALLOWED_METADATA_VERSIONS)):
-        #    raise ValueError(f"Metadata version {version} is not supported")
-
+        if not any(map(lambda x: re.match(x, version), ALLOWED_METADATA_VERSIONS)):
+            raise ValueError(f"Metadata version {version} is not supported")
         return version
 
     @property
