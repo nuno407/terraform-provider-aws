2f6a19ea63142b576d21899a99c88b10f69c962b
miguelmartins3 <106747499+miguelmartins3@users.noreply.github.com>
2024-02-26 09:40:25 +0000

    NO-TICKET: Fix mongodb installation script (#844)
    * NO-TICKET: Fix mongoDB installation script
    
    * NO-TICKET: Add support for Mac
    
    ---------
    
    Co-authored-by: Ângelo Sousa <angeloandre1998@gmail.com>

--- Selector/run_mongo_locally.sh
+++ Selector/run_mongo_locally.sh
@@ -2,8 +2,21 @@
 
 if ! [ -x "$(command -v mongod)" ];
 then
-    # THE INSTALL SCRIPT IS SPECIFIC TO UBUNTU 22.04, IT MIGHT NOT WORK FOR OTHER VERSIONS
-    # Refer to the official docs if it does not work https://www.mongodb.com/docs/v7.0/tutorial/install-mongodb-on-ubuntu/#std-label-install-mdb-community-ubuntu
+    # THE INSTALL SCRIPT IS SPECIFIC TO UBUNTU 22.04, 20.04 and MAC
+
+    # Check if is mac
+    if [ "$(uname -s)" == "Darwin" ]; then
+        brew install mongodb-community@7.0
+        exit
+    # Check if is ubuntu 22.04
+    elif [ "$(lsb_release -cs)" = "jammy" ]; then
+        echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+    # Check if is ubuntu 20.04
+    elif ["$(lsb_release -cs)" = "focal" ]; then
+        echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+    else
+        echo "Operating system not supported by this script, please refer to https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/ for specific instructions on how to install mongodb"
+        exit 1
 
     echo "MongoDB is not installed. Installing MongoDB..."
     # Install MongoDB
@@ -11,9 +24,9 @@ then
     curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
         sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg --dearmor
 
-
     sudo apt-get update
     sudo apt-get install -y mongodb-org=7.0.5 mongodb-org-database=7.0.5 mongodb-org-server=7.0.5 mongodb-mongosh=7.0.5 mongodb-org-mongos=7.0.5 mongodb-org-tools=7.0.5
+
 fi
     rm -f /tmp/mongodb.log
 
